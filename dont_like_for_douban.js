9db291696ad62d32a9e4a97c0824e3f95e156d1f
tingleshao <tingleshao@bitbucket.org/mkaplan/tabellaevictus.git>
2012-11-30 22:24:39 -0500

    persistence, cursor style

--- dont_like_for_douban.js
+++ dont_like_for_douban.js
@@ -1,16 +1,29 @@
 // ==UserScript==
-// @name                dont-like button for douban
+// @name                unlike button for douban
 // @namespace           tingleshao
-// @description         add don't like buttons for douban front page
+// @description         add unlike buttons for douban front page
 // @include             http://www.douban.com/
-// @version             1.0
+// @version             1.1
 // ==/UserScript==
-
-
 
 var guessList = document.getElementsByClassName('guess3-list')[0].childNodes;
 
+// close previous saved dont-like guess list
+
+var DL_list_s = GM_getValue("DL_list");
+if (typeof DL_list_s === "undefined") {
+    alert("ddl");
+    DL_list = [];
+}
+else 
+    DL_list = DL_list_s.split('&');
+
 for (var i = 1; i < guessList.length-1; i=i+2) {
+    for (var j = 0; j < DL_list.length; j=j+1) {
+        if (DL_list[j] == document.getElementsByClassName('guess3-list')[0].childNodes[i].getAttribute('unique_id')) {
+            document.getElementsByClassName('guess3-list')[0].childNodes[i].style.display = 'none';
+        }    
+    }
     var closeButton = document.createElement("cb");
     closeButton.id = "close_button" + i.toString();
     closeButton.href = "javascript";
@@ -30,6 +43,7 @@ for (var i = 1; i < guessList.length-1; i=i+2) {
           width:24px;\
           color: #CA6445;\
           background: #FAE9DA;\
+          cursor: pointer;\
      	}\
      '
     style = style.replace('&',i.toString());
@@ -37,23 +51,24 @@ for (var i = 1; i < guessList.length-1; i=i+2) {
     document.getElementsByClassName('guess3-list')[0].childNodes[i].appendChild(closeButton);
 }
 
-
 function closeGuess(id){
     var i = parseInt(id.split('n')[1]);
-	document.getElementsByClassName('guess3-list')[0].childNodes[i].style.display = 'none';
+	document.getElementsByClassName('guess3-list')[0].childNodes[i].style.display = 'none';   
+    DL_list.push(document.getElementsByClassName('guess3-list')[0].childNodes[i].getAttribute('unique_id'));
+    if (DL_list.length > 20)
+        DL_list = DL_list.slice(1,DL_list.length);
+    new_DL_list_s = ""
+    for (var i = 0; i < DL_list.length; i++) {
+        new_DL_list_s = new_DL_list_s + DL_list[i];
+        new_DL_list_s = new_DL_list_s + '&';
+    }
+    GM_setValue("DL_list",new_DL_list_s);
 }
 
 for (var i = 1; i < guessList.length-1; i=i+2) {
     cbb = document.getElementById("close_button"+i.toString());
     cbb.addEventListener("click",function(){closeGuess(this.id)}, false);
 }
-
-
-
-
-
-
-
 
 
 
